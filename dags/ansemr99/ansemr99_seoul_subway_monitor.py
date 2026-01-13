2b236154fec5b7eaba8823642c6af901a1e6d0aa
seungwoo <136046385+walter230@users.noreply.github.com>
2026-01-13 15:32:18 +0900

    Comment out Slack notification in subway monitor DAG

--- dags/ansemr99/ansemr99_seoul_subway_monitor.py
+++ dags/ansemr99/ansemr99_seoul_subway_monitor.py
@@ -96,16 +96,16 @@ with DAG(
         return record_count
 
     # 2. 슬랙 알림 태스크
-    send_slack_notification = SlackAPIPostOperator(
-        task_id='send_slack_notification',
-        slack_conn_id='ansemr99_slack_conn',
-        channel='#bot-playground',
-        text=":white_check_mark: *지하철 데이터 적재 완료*\n"
-             "- 대상 테이블: `realtime_subway_positions_2`\n"
-             "- 적재된 레코드 수: {{ task_instance.xcom_pull(task_ids='collect_and_insert_subway_data') }}개\n",
-        username='승우봇'
-    )
+    # send_slack_notification = SlackAPIPostOperator(
+    #     task_id='send_slack_notification',
+    #     slack_conn_id='ansemr99_slack_conn',
+    #     channel='#bot-playground',
+    #     text=":white_check_mark: *지하철 데이터 적재 완료*\n"
+    #          "- 대상 테이블: `realtime_subway_positions_2`\n"
+    #          "- 적재된 레코드 수: {{ task_instance.xcom_pull(task_ids='collect_and_insert_subway_data') }}개\n",
+    #     username='승우봇'
+    # )
 
     ingestion_task = collect_and_insert_subway_data()
 
-    ingestion_task >> send_slack_notification
+    # ingestion_task >> send_slack_notification
